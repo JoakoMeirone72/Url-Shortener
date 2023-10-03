897aea573fe564b63125d6d97395f737c853362c
Joako Meirone <joakomeirone@gmail.com>
2023-10-03 20:22:02 -0300

    Agregar archivos de proyecto.

---
+++ Url Shortener/Controllers/XYZController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Url_Shortener.Controllers
+{
+    public class XYZController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
